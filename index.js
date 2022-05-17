73d3310cc517db4b2787349a69a153a62a57fa64
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-05-17 00:25:55 -0700

    changed status code for registering user

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.post('/users',
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) => { res.status(200).json(user) })
+          .then((user) => { res.status(201).json(user) })
           .catch((error) => {
             console.error(error);
             res.status(500).send('Error:' + error);
