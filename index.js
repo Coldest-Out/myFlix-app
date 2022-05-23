7eeb77135cbeba68994b2a581bac21315c93bdc6
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-05-23 00:26:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ app.get(
   Email: String, (required)
   Birthday: Date (required)
 } */
-/* app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
+app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
 [
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
@@ -226,39 +226,6 @@ app.get(
       res.status(404).send('Error: ' + err);
     } else {
       res.json(updatedUser);
-    }
-  });
-}); */
-app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
-[
-  check('Username', 'Username is required').isLength({min: 5}),
-  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-  check('Password', 'Password id required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail()
-], (req, res) => {
-
-  // check the validation object for errors
-  let errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-    {
-      Username: req.body.Username,
-      Password: hashedPassword,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    }
-  },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if (user) {
-      res.status(201).json(updatedUser);
-    } else {
-      res.status(404).send('User Not Found');
     }
   });
 });
