c3d562087e888b0f58aa6b41fb8d14b6f56c480b
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-05-26 01:47:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:52929', 'https://cold-myflix-app.herokuapp.com'];
+let allowedOrigins = ['http://localhost:52929', 'https://cold-myflix-app.herokuapp.com', 'http://localhost:62802/'];
 
 app.use(cors({
   origin: (origin, callback) => {
