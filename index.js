d3f0cc69e546a28cf71f06ee10e62ca1d66e6c5a
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-05-29 21:32:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (_req, res) => {
 });
 
 //Gets list of all movies
-app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
